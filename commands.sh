6882cfee1830d78a2ddd3bfc4d07a61a9255a9c1
Anoushkrit Goel <anoushkritgoel@gmail.com>
2024-10-15 14:15:37 +0530

    Create commands.sh
    just some commands being used to setup up the total docker container

---
+++ commands.sh
@@ -0,0 +1,6 @@
+sudo docker pull scilus/scilus:latest
+sudo docker run -d -it scilus/scilus:latest
+sudo docker run -d -v /s3/docker_storage/scilus:/home/ --name neuro-scilus scilus/scilus:latest tail -f /dev/null
+sudo docker run -d --gpus all --memory=48g --cpus=all -v /s3/docker_storage/scilus:/home/ --name neuro-scilus scilus/scilus:latest tail -f /dev/null
+sudo docker exec -it neuro-scilus bash
+sudo docker stas
