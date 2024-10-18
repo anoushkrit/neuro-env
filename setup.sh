a96d424df34c0eb7df9c510fd6299d3d75ab1038
Anoushkrit Goel <anoushkritgoel@gmail.com>
2024-10-18 18:58:48 +0530

    Update setup.sh

--- setup.sh
+++ setup.sh
@@ -7,4 +7,8 @@ chmod +x nextflow
 mkdir -p $HOME/.local/bin/
 mv nextflow $HOME/.local/bin/
 apt-get install libgl1-mesa-glx libegl1-mesa libxrandr2 libxrandr2 libxss1 libxcursor1 libxcomposite1 libasound2 libxi6 libxtst6
-curl -O https://repo.anaconda.com/archive/Anaconda3-latest-Linux-x86_64.sh
+cd $HOME
+mkdir -p ~/miniconda3
+wget https://repo.anaconda.com/miniconda/Miniconda3-latest-Linux-x86_64.sh -O ~/miniconda3/miniconda.sh
+bash ~/miniconda3/miniconda.sh -b -u -p ~/miniconda3
+rm ~/miniconda3/miniconda.sh
