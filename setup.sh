f6f39a7fc2c607c14302c479a3f09c0a890e111e
Anoushkrit Goel <anoushkritgoel@gmail.com>
2024-10-21 18:04:25 +0530

    Update setup.sh

--- setup.sh
+++ setup.sh
@@ -1,11 +1,14 @@
+# setup curl
 apt-get install update 
 apt-get install curl zip
 curl -s https://get.sdkman.io | bash
 source ~/.bashrc
+# setup nextflow
 curl -s https://get.nextflow.io | bash
 chmod +x nextflow
 mkdir -p $HOME/.local/bin/
 mv nextflow $HOME/.local/bin/
+# setup conda
 apt-get install libgl1-mesa-glx libegl1-mesa libxrandr2 libxrandr2 libxss1 libxcursor1 libxcomposite1 libasound2 libxi6 libxtst6
 cd $HOME
 mkdir -p ~/miniconda3
@@ -14,4 +17,6 @@ bash ~/miniconda3/miniconda.sh -b -u -p ~/miniconda3
 rm ~/miniconda3/miniconda.sh
 cd ~
 source ~/.bashrc
+# setup scilpy
+conda env create -n scilus python=3.10.12
 
