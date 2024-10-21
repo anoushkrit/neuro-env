0e4b7f1aa707a4bf783fa271dfe67e55ed8b14df
Anoushkrit Goel <anoushkritgoel@gmail.com>
2024-10-22 00:13:23 +0530

    Update setup_scilus.sh

--- setup_scilus.sh
+++ setup_scilus.sh
@@ -1,12 +1,15 @@
 sudo apt install libblas-dev liblapack-dev gfortran
 sudo apt-get install git g++ python libeigen3-dev zlib1g-dev libqt5opengl5-dev libqt5svg5-dev libgl1-mesa-dev libfftw3-dev libtiff5-dev libpng-dev
 sudo apt-get install python3-distutils build-essential
+sudo apt install libfreetype6-dev
 ENV=tractolearn
 PROGRAM_PATH=\neuro\programs
 conda create -n $ENV python=3.10 && source ~/.bashrc
 conda activate ${ENV}
+#mrtrix
 conda install -c mrtrix3 mrtrix3
 conda update -c mrtrix3 mrtrix3
+#scilpy
 pip install --upgrade setuptools wheel
 pip install numpy
 git clone https://github.com/getspams/spams-python && cd spams-python && pip install -e .
@@ -15,5 +18,7 @@ cd $PROGRAM_PATH
 git clone https://github.com/scilus/scilpy.git
 export SETUPTOOLS_USE_DISTUTILS=stdlib
 cd scilpy/ && pip install -e .
+# dmriQCpy
+pip install git+https://github.com/scilus/dmriqcpy.git
 
 
