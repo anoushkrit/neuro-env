6a2c07cbf785c3cfc1fa0b66ade0e26d0f1e294d
Anoushkrit Goel <anoushkritgoel@gmail.com>
2024-10-21 18:00:46 +0530

    Update commands.sh

--- commands.sh
+++ commands.sh
@@ -3,4 +3,8 @@ sudo docker run -d -it scilus/scilus:latest
 sudo docker run -d -v /s3/docker_storage/scilus:/home/ --name neuro-scilus scilus/scilus:latest tail -f /dev/null
 sudo docker run -d --gpus all --memory=48g --cpus=all -v /s3/docker_storage/scilus:/home/ --name neuro-scilus scilus/scilus:latest tail -f /dev/null
 sudo docker exec -it neuro-scilus bash
-sudo docker stas
+sudo docker stats
+
+
+
+sudo docker run -d -v /s3/docker_storage/scilus:/home/ --name neuro-scilus scilus/scilus:latest tail -f /dev/null
